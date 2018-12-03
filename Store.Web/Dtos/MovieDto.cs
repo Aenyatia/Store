c551277f169e6b386408ea82689c91443d3692d9
Aenyatia Nirvalen <aenyatia@gmail.com>
2018-12-03 17:33:39 +0100

    Add genre dto.

---
+++ Store.Web/Dtos/MovieDto.cs
@@ -0,0 +1,16 @@
+﻿using Store.Core;
+using System;
+
+namespace Store.Web.Dtos
+{
+	public class MovieDto
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public byte GenreId { get; set; }
+		public Genre Genre { get; set; }
+		public DateTime AddedAt { get; set; }
+		public DateTime Release { get; set; }
+		public int NumberInStock { get; set; }
+	}
+}
